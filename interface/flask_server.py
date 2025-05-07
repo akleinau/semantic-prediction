ad702083f7bab649af2f090e09d1a52b93e13d55
akleinau <56925445+akleinau@users.noreply.github.com>
2025-05-07 15:29:31 +0200

    add input params

--- interface/flask_server.py
+++ interface/flask_server.py
@@ -1,12 +1,37 @@
 from flask import Flask, request
+from flask_cors import CORS
+
 from predict import predict, InputClass
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/predict", methods=['POST'])
 def predict():
     data = request.get_json()
     input = InputClass(data.input)
     return predict(input)
+
+@app.route("/get_input_params")
+def get_input_params():
+    return {"params": [
+        {"id": "meanage", "label": "Mean Age", "type": "slider", "min": 15, "max": 80, "value": 50},
+        {"id": "proportionfemale", "label": "Proportion female", "type": "slider", "min": 0, "max": 100, "value": 50},
+        {"id": "meantobacco", "label": "Mean number of times tobacco used", "type": "slider", "min": 1, "max": 30, "value": 10},
+        {"id":"followup", "label": "Follow up (weeks)", "type": "slider", "min": 4, "max": 60, "value": 26},
+        {"id": "patientrole", "label": "Patient role?", "type": "checkbox", "value": 0},
+        {"id": "verification", "label": "Biochemical verification", "type": "checkbox", "value": 0},
+        {"id": "outcome", "label": "Outcome", "type": "select", "choices":
+            ["Abstinence: Continuous ", "Abstinence: Point Prevalence "]},
+        ],
+        "interventions": [
+        {"id": "intervention", "label": "Intervention", "type": "select", "choices": []},
+        {"id": "delivery", "label": "Delivery", "type": "select", "choices": []},
+        {"id": "source", "label": "Source", "type": "select", "choices": []}
+    ]}
+
+@app.route("/hello_world")
+def hello_world():
+    return {"text":"Hello, World!"}
 
 
