70db721b1b6440a1e9784e8a5a178e5cb21afaba
akleinau <56925445+akleinau@users.noreply.github.com>
2025-05-08 12:27:13 +0200

    add interventions to flask

--- interface/flask_server.py
+++ interface/flask_server.py
@@ -1,5 +1,25 @@
 from flask import Flask, request
 from flask_cors import CORS
+import numpy as np
+import pandas as pd
+
+import inspect
+import pathlib
+import pickle
+import sys
+import os
+
+src_file_path = inspect.getfile(lambda: None)
+
+PACKAGE_PARENT = pathlib.Path(src_file_path).parent
+PACKAGE_PARENT2 = PACKAGE_PARENT.parent
+#SCRIPT_DIR = PACKAGE_PARENT2 / "rulenn"
+#DATAPR_DIR = PACKAGE_PARENT2 / "dataprocessing"
+sys.path.append(str(PACKAGE_PARENT2))
+#sys.path.append(str(SCRIPT_DIR))
+#sys.path.append(str(DATAPR_DIR))
+
+from rulenn.rule_nn import RuleNNModel
 
 from predict import predict, InputClass
 
@@ -15,20 +35,53 @@ def predict_request():
 
 @app.route("/get_input_params")
 def get_input_params():
+
+    checkpoint = 'examples/model_final.json'
+    path = 'data/hbcp_gen.pkl'
+
+    model = RuleNNModel.load(checkpoint)
+    model.model.eval()  # Run in production mode
+
+    with open(path, "rb") as fin:
+        raw_features, raw_labels = pd.read_pickle(fin)
+    raw_features[np.isnan(raw_features)] = 0
+
+    features = raw_features
+
+    # Additional filter based on the loaded model. Maybe the only one really needed?
+    retainedfeatures = [x for x in features.columns if x[1] in model.variables]
+    features = features[retainedfeatures]
+
+    featurenames = [x[1] for x in features.columns]
+    featuresemantics = pd.read_csv('data/feature-semantics.csv')
+
+    print("We have ", len(featurenames), " features.")
+
+    intervention = featuresemantics.query('group == "intervention"')['featurename'].values.tolist()
+    intervention = [x for x in intervention if x in featurenames]
+    delivery = featuresemantics.query('group == "deliverymode"')['featurename'].values.tolist()
+    delivery = [x for x in delivery if x in featurenames]
+    source = featuresemantics.query('group == "deliverysource"')['featurename'].values.tolist()
+    source = [x for x in source if x in featurenames]
+    pharmacological = featuresemantics.query('group == "pharmacological"')['featurename'].values.tolist()
+    pharmacological = [x for x in pharmacological if x in featurenames]
+    outcome = featuresemantics.query('group == "outcome"')['featurename'].values.tolist()
+    outcome = [x for x in outcome if x in featurenames]
+
+
     return {"params": [
-        {"id": "meanage", "label": "Mean Age", "type": "slider", "min": 15, "max": 80, "value": 50},
-        {"id": "proportionfemale", "label": "Proportion female", "type": "slider", "min": 0, "max": 100, "value": 50},
-        {"id": "meantobacco", "label": "Mean number of times tobacco used", "type": "slider", "min": 1, "max": 30, "value": 10},
-        {"id":"followup", "label": "Follow up (weeks)", "type": "slider", "min": 4, "max": 60, "value": 26},
+        {"id": "meanage", "label": "Mean Age", "type": "slider", "min": 15, "max": 80, "value": 50, "step": 1},
+        {"id": "proportionfemale", "label": "Proportion female", "type": "slider", "min": 0, "max": 100, "value": 50, "step": 1},
+        {"id": "meantobacco", "label": "Mean number of times tobacco used", "type": "slider", "min": 1, "max": 30, "value": 10, "step": 1},
+        {"id":"followup", "label": "Follow up (weeks)", "type": "slider", "min": 4, "max": 60, "value": 26, "step": 1},
         {"id": "patientrole", "label": "Patient role?", "type": "checkbox", "value": 0},
         {"id": "verification", "label": "Biochemical verification", "type": "checkbox", "value": 0},
-        {"id": "outcome", "label": "Outcome", "type": "select", "choices":
-            ["Abstinence: Continuous ", "Abstinence: Point Prevalence "], "value": "Abstinence: Continuous "},
+        {"id": "outcome", "label": "Outcome", "type": "select", "choices": outcome, "value": "Abstinence: Continuous "},
         ],
         "interventions": [
-        {"id": "intervention", "label": "Intervention", "type": "select", "choices": [], "value": []},
-        {"id": "delivery", "label": "Delivery", "type": "select", "choices": [], "value": []},
-        {"id": "source", "label": "Source", "type": "select", "choices": [], "value": []}
+        {"id": "intervention", "label": "Intervention", "type": "multiselect", "choices": intervention, "value": []},
+        {"id": "delivery", "label": "Delivery", "type": "multiselect", "choices": delivery, "value": []},
+        {"id": "source", "label": "Source", "type": "multiselect", "choices": source, "value": []}
     ]}
 
 @app.route("/hello_world")
